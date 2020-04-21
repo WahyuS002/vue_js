ed0d751a06a5e2af16674d0866801fe9da95cf79
WahyuS002 <syahputrawahyu61@gmail.com>
2020-04-21 22:07:15 +0700

    computed properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
-new Vue({
+var vo = new Vue({
     el: '#app',
     data: {
-        show: true,
-        nama: "Wahyu Syahputra",
+        nama: "wahyu",
         umur: 19
+    },
+    // Computed basic
+    // computed: {
+    //     biodata: function () {
+    //         return this.nama + " umurnya " + this.umur
+    //     }
+    // }
+
+    // Computed advanced (get, post)
+    computed: {
+        biodata: {
+            get: function () {
+                return this.nama + " umurnya " + this.umur
+            },
+            set: function (parameter) {
+                var ubah = parameter.split(' ');
+                this.nama = ubah[0]
+                this.umur = ubah[1]
+            }
+        }
     }
 });
