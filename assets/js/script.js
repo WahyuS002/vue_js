1d32c077b4d80fa8cb5bc8bea0b67266f3b050db
WahyuS002 <syahputrawahyu61@gmail.com>
2020-04-22 09:59:40 +0700

    Event Handling : v-on & methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,13 @@ var vo = new Vue({
     el: '#app',
     data: {
 
-        // Objek biasa
-
-        students: {
-            nama: "Objek",
-            umur: 001
+    },
+    methods: {
+        klik: function () {
+            alert("hello")
         },
-
-        // Array yang menampung objek
-        members: [{
-                nama: "Wahyu",
-                umur: 19
-            },
-            {
-                nama: "Antok",
-                umur: 220
-            },
-            {
-                nama: "Rey",
-                umur: 123
-            }
-        ]
-
+        input: function (params) {
+            alert(params)
+        }
     }
 });
